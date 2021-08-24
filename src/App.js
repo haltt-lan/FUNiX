5dca40c6cf2c0c924f199823293eeb371257ce48
haltt-lan <lethuha.lann@gmail.com>
2021-08-24 13:42:04 +0700

    add file my-test-app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+// import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponents';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Trang chủ</NavbarBrand>
+
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
