1b9a68792b367640d37e36370ae7ddf7bd54b211
haltt-lan <lethuha.lann@gmail.com>
2021-10-12 17:01:18 +0700

    add lab03- part 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
+import React, { Component } from 'react';
 import MenuComponent from './component/MenuComponent';
+import { DISHES } from './shared/dishes';
+import {Navbar, NavbarBrand} from 'reactstrap'
 
-function App() {
-  return (
-    <div>
-      <MenuComponent />
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <MenuComponent dishes={this.state.dishes} />
+      </div>
+    )
 }
-
-export default App;
+}
