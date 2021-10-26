847db302ade9d2a1b8da68ab76660faf02318068
haltt-lan <lethuha.lann@gmail.com>
2021-10-26 09:02:21 +0700

    add lab11-1

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -9,6 +9,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const mapStateToProps = state => {
   return {
@@ -20,18 +21,19 @@ const mapStateToProps = state => {
 }
 const mapDispatchToProps = dispatch => ({
   postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())},
-  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
-  fetchComments: () => { dispatch(fetchComments())},
-  fetchPromos: () => { dispatch(fetchPromos())},
-  fetchLeaders: () => { dispatch(fetchLeaders())}
+  fetchDishes: () => { dispatch(fetchDishes()) },
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
+  fetchComments: () => { dispatch(fetchComments()) },
+  fetchPromos: () => { dispatch(fetchPromos()) },
+  fetchLeaders: () => { dispatch(fetchLeaders()) }
 });
 
 
 class Main extends Component {
 
+
   //dispatch (dishesLoading(true)) , 2s sau dispatch (addDishes(DISHES))
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
@@ -39,46 +41,51 @@ class Main extends Component {
   }
   render() {
     const HomePage = () => {
-      return(
-        <Home 
-        dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} // lúc này dishes = [] chưa có dữ liệu
-        dishesLoading={this.props.dishes.isLoading} // lúc này isLoading = true, hiển thị biểu tượng Loading
-        dishesErrMess={this.props.dishes.errMess} // lúc này errMess = null
-        promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
-        promosLoading={this.props.promotions.isLoading} // lúc này isLoading = true, hiển thị biểu tượng Loading
-        promosErrMess={this.props.promotions.errMess}
-        leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
-        leadersLoading={this.props.leaders.isLoading} // lúc này isLoading = true, hiển thị biểu tượng Loading
-        leadersErrMess={this.props.leaders.errMess}
-    />
+      console.log('location',this.props.location)
+      return (
+        <Home
+          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} // lúc này dishes = [] chưa có dữ liệu
+          dishesLoading={this.props.dishes.isLoading} // lúc này isLoading = true, hiển thị biểu tượng Loading
+          dishesErrMess={this.props.dishes.errMess} // lúc này errMess = null
+          promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+          promosLoading={this.props.promotions.isLoading} // lúc này isLoading = true, hiển thị biểu tượng Loading
+          promosErrMess={this.props.promotions.errMess}
+          leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+          leadersLoading={this.props.leaders.isLoading} // lúc này isLoading = true, hiển thị biểu tượng Loading
+          leadersErrMess={this.props.leaders.errMess}
+        />
       );
     }
-    const DishWithId = ({match}) => {
-      console.log("dishWithId:",match.params.dishId)
-      return(
-        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-        isLoading={this.props.dishes.isLoading}
-        errMess={this.props.dishes.errMess}
-        comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-        postComment={this.props.postComment}
-        commentsErrMess={this.props.comments.errMess}
-      />
+    const DishWithId = ({ match }) => {
+      console.log("dishWithId:", match.params.dishId)
+      return (
+        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
+          comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+          postComment={this.props.postComment}
+          commentsErrMess={this.props.comments.errMess}
+        />
       );
     };
     return (
       <div>
         <Header />
-        <Switch>
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+            <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
               <Redirect to="/home" />
-          </Switch>
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
         <Footer />
       </div>
     );
   }
 }
 
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
